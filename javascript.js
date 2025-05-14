b285d86e9830e6c8ead48624fa4834634fa04fac
siliciummoss <robin.grotheer307@gmail.com>
2025-05-14 23:17:27 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 
 const imageList = [
     "gallery/Me_and_my_best_friend_Paul_again.jpg",
-    "gallery/Rebuilding_my_farm!.jpg",
+    //"gallery/Rebuilding_my_farm!.jpg",
     "gallery/Were_going_on_a_trip.jpg"
   ];
 
@@ -21,10 +21,17 @@ imageList.forEach(path => {
     gallery.appendChild(item);
 });
 
+const item = document.createElement('div');
+    item.className = 'gallery-item-more';
+    item.innerHTML = `
+      <img src="items/more_full.png" alt="more">
+      <div class="caption"></div>
+    `;
+    gallery.appendChild(item);
+
 
 const embedList = [
     { url: "https://medal.tv/de/games/dredge/clip/kdkfzj8ClSYB4ujIG?invite=cr-MSxkbVcsMzgwOTI2NTc5", caption: "That_was_pretty_close..."},
-    { url: "https://medal.tv/de/games/repo/clip/k0wmr1zytUrpbEZiu?invite=cr-MSwzcnIsMzgwOTI2NTc5", caption: "Giant enemy spider"},
     { url: "https://medal.tv/de/games/repo/clip/k07trPhQgycJoh1Rh?invite=cr-MSxOUDYsMzgwOTI2NTc5", caption: "AHHHHHHHHHHhhhh"}
 ];
 
@@ -34,23 +41,31 @@ const embedList = [
 const embedGallery = document.getElementById('embedGallery');
 
 embedList.forEach(item => {
-const caption = item.caption.replace(/_/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
+  const caption = item.caption.replace(/_/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
+
+  const div = document.createElement('div');
+  div.className = 'gallery-item';
+
+  let embedHTML = '';
+  if (item.url.includes("medal.tv")) {
+        embedHTML = `<iframe width="640" height="360" style="border: none;" src="${item.url}" frameborder="0" allowfullscreen></iframe>`;
+      } else if (item.type === "pdf") {
+        embedHTML = `<embed src="${item.url}" type="application/pdf">`;
+      } else if (item.type === "video") {
+        embedHTML = `<video controls><source src="${item.url}" type="video/mp4"></video>`;
+      }
+
+      div.innerHTML = `
+        <div class="embed-container">${embedHTML}</div>
+        <div class="caption">${caption}</div>
+      `;
+      embedGallery.appendChild(div);
+});
 
 const div = document.createElement('div');
-div.className = 'gallery-item';
-
-let embedHTML = '';
-if (item.url.includes("medal.tv")) {
-      embedHTML = `<iframe width="640" height="360" style="border: none;" src="${item.url}" frameborder="0" allowfullscreen></iframe>`;
-    } else if (item.type === "pdf") {
-      embedHTML = `<embed src="${item.url}" type="application/pdf">`;
-    } else if (item.type === "video") {
-      embedHTML = `<video controls><source src="${item.url}" type="video/mp4"></video>`;
-    }
-
+    div.className = 'gallery-item-more';
     div.innerHTML = `
-      <div class="embed-container">${embedHTML}</div>
-      <div class="caption">${caption}</div>
+      <div class="embed-container"><img src="items/more_full.png" alt="more" style="width=640; height=360; border: none;"></div>
+      <div class="caption"></div>
     `;
     embedGallery.appendChild(div);
-});
